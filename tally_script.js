68559de68b239956039ce70de251149c575864b9
Alex McFadyen <alex@demotix.com>
2015-02-22 13:08:33 -0800

    Adding re-calculate function and moving tally to button bar.

--- tally_script.js
+++ tally_script.js
@@ -1,62 +1,63 @@
 // Hack to detect if its already been run
-if (!$("span.tallyTitle").length) {
+if ($("span.tallyTitle").length) {
+  $("span.tallyTitle").remove();
+  $("li.tallyedPins").remove();
+  $("div.tally").remove();
+};
 
-  var pins = $('div.item').has('div.pinHolder');
+var pins = $('div.item').has('div.pinHolder');
 
-  var total = 0;
+var total = 0;
 
-  // tallyTitle
-  var tallyTitle = '<span class="tallyTitle" style="color:#156584; font-size:0.8em; margin-left:30px"><img style="padding-bottom: 6px" src="https://rawgit.com/TallyApp/code/master/icons/tally_icon_32x32.png"> <span class="tallyPrice" /></span>';
-  $('h1.boardName').append(tallyTitle);
-  $('h2.smallBoardName').append(tallyTitle);
+// tallyTitle
+var tallyTitle = '<span class="tallyTitle" style="color:#156584; margin-right:4px; font-size:1.1em; position:relative; top:2px;"><img style="height: 22px; padding-bottom: 4px;" src="https://rawgit.com/TallyApp/code/master/icons/tally_icon_32x32.png"> <span class="tallyPrice" /></span>';
+$('div.boardButtons').prepend(tallyTitle);
 
-  // tallyBar
-  var barTemplate = '<span class="value"></span><span class="label"></span>';
-  $('div.pinsAndFollowerCount').css('width', '300px');
-  $('div.pinsAndFollowerCount ul').append('<li class="tallyedPins">'+ barTemplate +'</li>');
+// tallyBar
+var barTemplate = '<span class="value"></span><span class="label"></span>';
+$('div.pinsAndFollowerCount').css('width', '300px');
+$('div.pinsAndFollowerCount ul').append('<li class="tallyedPins">'+ barTemplate +'</li>');
 
-  $('li.tallyedPins span.label').html('Pins Tallied');
-  $('li.tallyedPins span.value').html(pins.length);
+$('li.tallyedPins span.label').html('Pins Tallied');
+$('li.tallyedPins span.value').html(pins.length);
 
-  var tallyDiv = "<div class='tally' style='float: right; margin: 15px 5px 5px 5px; font-size: 14px'><img src='https://rawgit.com/TallyApp/code/master/icons/tally_icon_16x16.png'> <span class='price'>";
+var tallyDiv = "<div class='tally' style='float: right; margin: 15px 5px 5px 5px; font-size: 13px; font-weight: normal;'><img style='padding-bottom: 3px;' src='https://rawgit.com/TallyApp/code/master/icons/tally_icon_16x16.png'> <span class='price'>";
 
-  pins.each(function( index, pin ) {
-    // Rich pin lookup
-    if ($('a.richPinMetaLink', pin).length) {
+pins.each(function( index, pin ) {
+  // Rich pin lookup
+  if ($('a.richPinMetaLink', pin).length) {
 
-      var url = 'https://www.pinterest.com' + $('a.richPinMetaLink', pin).attr('href');
+    var url = 'https://www.pinterest.com' + $('a.richPinMetaLink', pin).attr('href');
 
-      $.ajax({
-        type: 'GET',
-        url: url,
-        dataType: "json",
-        success: function(data) {
-          var product = data.resource_data_cache[0].data.rich_metadata.products[0];
-          if (product) {
-            var price = product.offer_summary.price;
+    $.ajax({
+      type: 'GET',
+      url: url,
+      dataType: "json",
+      success: function(data) {
+        var product = data.resource_data_cache[0].data.rich_metadata.products[0];
+        if (product) {
+          var price = product.offer_summary.price;
 
-            total = total + Number(price.substring(1));
-
-            $('div.pinWrapper div.pinCredits', pin).prepend(tallyDiv + price + '</div></div>');
-
-            $('span.tallyTitle span.tallyPrice').html('$' + total.toFixed(2));
-          }
-        }
-      });
-    }
-    // Otherwise try to pull price out of the description
-    else {
-      var description = $('p.pinDescription', pin).html();
-      if (description){
-        var priceMatch = description.match(/[\W\D]\d+(\.\d+)?/);
-        if (priceMatch) {
-          var price = priceMatch[0];
           total = total + Number(price.substring(1));
+
           $('div.pinWrapper div.pinCredits', pin).prepend(tallyDiv + price + '</div></div>');
+
           $('span.tallyTitle span.tallyPrice').html('$' + total.toFixed(2));
         }
       }
+    });
+  }
+  // Otherwise try to pull price out of the description
+  else {
+    var description = $('p.pinDescription', pin).html();
+    if (description){
+      var priceMatch = description.match(/[\W\D]\d+(\.\d+)?/);
+      if (priceMatch) {
+        var price = priceMatch[0];
+        total = total + Number(price.substring(1));
+        $('div.pinWrapper div.pinCredits', pin).prepend(tallyDiv + price + '</div></div>');
+        $('span.tallyTitle span.tallyPrice').html('$' + total.toFixed(2));
+      }
     }
-  });
-
-}
+  }
+});
